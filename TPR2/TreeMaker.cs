d22d8106bfe36f400686663163edc5bddf52df4d
albina6 <m_ab3@mail.ru>
2019-11-02 18:38:27 +0500

    чисто

--- TPR2/TreeMaker.cs
+++ TPR2/TreeMaker.cs
@@ -26,9 +26,7 @@ namespace TPR2
             5//если больше ?
         };
 
-        // Dictionary<Button,GroupBox> childDict;
         Dictionary<Button, Node> sitsLog;
-       // Dictionary<Button, Node> _sits;
         Dictionary<Button, Node> sitsPlus;
         Dictionary<TextBox, Node> sitsName;
         Dictionary<TextBox, Node> sitsProp;
@@ -37,7 +35,6 @@ namespace TPR2
         public TreeMaker()
         {
             InitializeComponent();
-           // childDict = new Dictionary<Button, GroupBox>();
             sitsLog = new Dictionary<Button, Node>();
             sitsPlus = new Dictionary<Button, Node>();
             sitsName = new Dictionary<TextBox, Node>();
@@ -63,16 +60,6 @@ namespace TPR2
             sitsLog.Add(logButton, _node);
             while (countChildren< _node.Down.Count())
             {
-                //if (countChildren == 0)
-                //{
-                //Button logButton = new Button
-                //{
-                //    Top = logB.Top + 40,
-                //    Left = logB.Left + 30,//что-нибудь про размер
-                //    Text = _node.Sel.ToString()
-                //};
-                //sitsLog.Add(logButton, _node);
-                //}
                 Node child =  _node.Down.ElementAt(countChildren);
                 TextBox name = new TextBox
                 {
@@ -102,16 +89,12 @@ namespace TPR2
                 };
 
                 add.MouseClick += plusButton_Click;
-                //sitsLog[logB].Down.Add(newsit);
-
                 name.TextChanged += name_Changed;
                 prob.TextChanged += prop_Changed;
 
                 sitsName.Add(name, child);
                 sitsPlus.Add(add, child);
                 sitsProp.Add(prob, child);
-               // listNode.Add(newsit);
-
                 this.Controls.Add(name);
                 this.Controls.Add(prob);
                 this.Controls.Add(add);
@@ -121,16 +104,6 @@ namespace TPR2
                     dravListNodes(listN, add);
 
             }
-
-            
-            //listNode.Add(_node);
-
-            //listN.RemoveAt(0);
-            //if (listN.Count() > 0)
-            //{
-
-            //}
-
         }
         private void makeNewGroupBox(object sender, EventArgs e)
         {
@@ -175,59 +148,6 @@ namespace TPR2
             this.Controls.Add(name);
             this.Controls.Add(prob);
             this.Controls.Add(add);
-
-            //Button logicalButton = sender as Button;
-            //Node newNode = new Node(0, "", sitsLog[logicalButton].Depth + 1);
-            ////GroupBox gb = new GroupBox()
-            ////{
-            ////    Top = logicalButton.Top + 40,
-            ////    Left = logicalButton.Left + (sitsLog[logicalButton].Down.Count - 1) * brims[sitsLog[logicalButton].Depth],
-            ////    Width = logicalButton.Width,
-            ////    Height = logicalButton.Height
-            ////};
-            //TextBox name = new TextBox
-            //{
-            //    Top=logicalButton.Top + 40,
-            //    Left=logicalButton.Left+( sitsLog[logicalButton].Down.Count-1)*brims[sitsLog[logicalButton].Depth],
-            //    Width = logicalButton.Width,
-            //    Height = logicalButton.Height
-            //};
-            //TextBox prop = new TextBox
-            //{
-            //    Top = name.Top + name.Height + 5,
-            //    Left = name.Left,
-            //    Width = logicalButton.Width,
-            //    Height = logicalButton.Height
-            //};
-
-            //Button plusBut = new Button
-            //{
-            //    Top = prop.Top + prop.Height + 1,
-            //    Left = name.Left,
-            //    Width = logicalButton.Width,
-            //    Height = logicalButton.Height,
-            //    Text="+",
-            //};
-            //plusBut.MouseClick += plusButton_Click;
-            //sitsLog[logicalButton].Down.Add(newNode);
-
-            //name.TextChanged += name_Changed;
-            //prop.TextChanged += prop_Changed;
-
-            //sitsLog[logicalButton].Down.Add(newNode);
-
-            //name.TextChanged += name_Changed;
-            //prop.TextChanged += prop_Changed;
-
-            //sitsName.Add(name, newNode);
-            //sitsPlus.Add(plusBut, newNode);
-            //sitsProp.Add(prop, newNode);
-
-            //this.Controls.Add(name);
-            //this.Controls.Add(prop);
-            //this.Controls.Add(plusBut);
-
-            //this.Controls.Add(gb);
         }
 
         private void plusButton_Click(object sender, EventArgs e)
@@ -256,16 +176,11 @@ namespace TPR2
             logButton.Text = s.ToString();
             logButton.Click += makeNewGroupBox;
             this.Controls.Add(logButton);
-            //создание нового groupBox
-            
-            //childDict.Add(logButton,);
-
         }
 
         private void name_Changed(object sender, EventArgs e) => sitsName[sender as TextBox].Name = (sender as TextBox).Text;
 
         private void prop_Changed(object sender, EventArgs e)
-        // => sitsProp[sender as TextBox].Property = double.Parse((sender as TextBox).Text.Replace(".", ","));
         {
             try
             {
@@ -284,9 +199,6 @@ namespace TPR2
             s.textBox1.Text = _root.Falb().Replace("!!", "");
             s.textBox2.Text = _root.Fal();
             double prop = _root.CalculateProp();
-            //s.textBox1.Text = _root.Falb().Replace("!!", "");
-            //s.textBox2.Text = _root.Fal();
-            //double prop = _root.CalculateProp();
             s.textBox4.Text = prop.ToString();
         }
 
@@ -331,72 +243,16 @@ namespace TPR2
             }
         }
 
-        //private void save_Click(object sender, EventArgs e)
-        //{
-        //    TextWriter writer = null;
-        //    try
-        //    {
-        //        var serializer = new XmlSerializer(typeof(Node));
-        //        writer = new StreamWriter("D:/pro/7sem/теория принятия решений/TPR2 - master/TPR2/bin/Debug/listNode.txt", false);
-        //        serializer.Serialize(writer, listNode);
-        //    }
-        //    finally
-        //    {
-        //        if (writer != null)
-        //            writer.Close();
-        //    }
-        //}
-
         private void download_Click(object sender, EventArgs e)
         {
-            //TextReader reader = null;
-            //try
-            //{
-            //    var serializer = new XmlSerializer(typeof(Node));
-            //    reader = new StreamReader("D:/pro/7sem/теория принятия решений/TPR2 - master/TPR2/bin/Debug/listNode");
-            //    listNode.Add( (Node)serializer.Deserialize(reader));
-            //}
-            //finally
-            //{
-            //    if (reader != null)
-            //        reader.Close();
-            //}
-
-
-
 
             if (string.IsNullOrEmpty("listNode"))
             {
-                // listNode.Clear();
+
             }
+
             else
             {
-               // listNode.Clear();
-                //Node objectOut = default(Node);
-
-                ////try
-                ////{
-                ////    XmlDocument xmlDocument = new XmlDocument();
-                ////    xmlDocument.Load("listNode");
-                ////    string xmlString = xmlDocument.OuterXml;
-
-                ////    using (StringReader read = new StringReader(xmlString))
-                ////    {
-                ////        Type outType = typeof(Node);
-
-                ////        XmlSerializer serializer = new XmlSerializer(outType);
-                ////        using (XmlReader reader = new XmlTextReader(read))
-                ////        {
-                ////            listNode.Add((Node)serializer.Deserialize(reader));
-                ////            //listNode=(Node)serializer.Deserialize(reader);
-                ////        }
-                ////    }
-                ////}
-                ////catch (Exception ex)
-                ////{
-                ////    //Log exception here
-                ////}
-
                 TextReader reader = null;
                 clear_Click(clear, e);
                 try
@@ -410,125 +266,21 @@ namespace TPR2
                     if (reader != null)
                         reader.Close();
                 }
-
-
-                // clear_Click(clear, e);
-                //nameTB.Text = "";
-                //propTB.Text = "";
-                /////////////////очистить экран
                 List<Node> liNode = new List<Node>(listNode);
-
-
-                //listNode =new List<Node>( liNode);
                 _root = liNode.First();
                 sitsName.Add(nameTB,_root);
                 sitsProp.Add(propTB, _root);
                 sitsPlus.Add(plusButton, _root);
-
-
-                // liNode.RemoveAt(0);////
                 nameTB.Text = _root.Name;
                 propTB.Text = _root.Property.ToString();
                 if (_root.Down.Count > 0)
                 {
-                    //Button logButton = new Button
-                    //{
-                    //    Top = plusButton.Top + 40,
-                    //    Left = plusButton.Left + 30,//что-нибудь про размер
-                    //    Text = _nodeMain.Sel.ToString()
-                    //};
-
-                    ////try
-                    ////{
-                    ////    sitsPlus.Add(plusButton, _nodeMain);
-                    ////}
-                    ////catch { }
-
-                    //sitsLog.Add(logButton, sitsPlus[plusButton]);
-                    ////sitsName.Add(nameTB, _nodeMain);
-                    ////sitsProp.Add(propTB, _nodeMain);
-
-                    // logButton.Click += makeNewGroupBox;
-                    // this.Controls.Add(logButton);
                     dravListNodes(liNode, plusButton);
                 }
                 else { }
-                ///// вызов метода отрисовки 
-
                 liNode.Clear();
             }
         }
-        //private void download_Click(object sender, EventArgs e)
-        //{
-
-        //    if (string.IsNullOrEmpty("listNode"))
-        //    {
-        //        // listNode.Clear();
-        //    }
-        //    else
-        //    {
-        //        listNode.Clear();
-        //        //Node objectOut = default(Node);
-
-        //        try
-        //        {
-        //            XmlDocument xmlDocument = new XmlDocument();
-        //            xmlDocument.Load("listNode");
-        //            string xmlString = xmlDocument.OuterXml;
-
-        //            using (StringReader read = new StringReader(xmlString))
-        //            {
-        //                Type outType = typeof(Node);
-
-        //                XmlSerializer serializer = new XmlSerializer(outType);
-        //                using (XmlReader reader = new XmlTextReader(read))
-        //                {
-        //                    listNode.Add((Node)serializer.Deserialize(reader));
-        //                    //listNode=(Node)serializer.Deserialize(reader);
-        //                }
-        //            }
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            //Log exception here
-        //        }
-        //        clear_Click(clear, e);
-        //        nameTB.Text = "";
-        //        propTB.Text = "";
-        //        /////////////////очистить экран
-        //        List<Node> liNode = new List<Node>(listNode);
-        //        Node _nodeMain = liNode.First();
-
-        //        // liNode.RemoveAt(0);////
-        //        nameTB.Text = _nodeMain.Name;
-        //        propTB.Text = _nodeMain.Property.ToString();
-        //        if (_nodeMain.Down.Count != 0)
-        //        {
-        //            Button logButton = new Button
-        //            {
-        //                Top = plusButton.Top + 40,
-        //                Left = plusButton.Left + 30,//что-нибудь про размер
-        //                Text = _nodeMain.Sel.ToString()
-        //            };
-        //            try
-        //            {
-        //                sitsPlus.Add(plusButton, _nodeMain);
-        //            }
-        //            catch { }
-        //            //sitsLog.Add(logButton, sitsPlus[plusButton]);
-        //            sitsName.Add(nameTB, _nodeMain);
-        //            sitsProp.Add(propTB, _nodeMain);
-
-        //            logButton.Click += makeNewGroupBox;
-        //            this.Controls.Add(logButton);
-        //            dravListNodes(liNode, logButton);
-        //        }
-        //        else { }
-        //        ///// вызов метода отрисовки 
-
-        //        liNode.Clear();
-        //    }
-        //    // return objectOut;
-        //}
+      
     }
 }
